40d639bd8394c2dd2a78ef46822d387fdfd86d4c
ahmfarisi <ahmfarisi@gmail.com>
2022-11-30 16:51:45 +0700

    Create QuoteModel.java

---
+++ app/src/main/java/com/ahmfarisi/myquote/Model/QuoteModel.java
@@ -0,0 +1,13 @@
+package com.ahmfarisi.myquote.Model;
+
+public class QuoteModel {
+    private String text, author;
+
+    public String getText() {
+        return text;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
